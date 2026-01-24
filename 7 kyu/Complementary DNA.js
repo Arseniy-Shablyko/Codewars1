0480fd8b2d0d7f5db0a7bf14bcccfcd0e1f6ea52
Arseniy-Shablyko <241208arseniy@gmail.com>
2026-01-25 02:16:39 +0300

    Create Complementary DNA.js

---
+++ 7 kyu/Complementary DNA.js
@@ -0,0 +1,15 @@
+function dnaStrand(dna){
+  let dnaArray = dna.split('');
+  for (let i = 0; i < dnaArray.length; i++) {
+    if (dnaArray[i] === "A") {
+      dnaArray[i] = "T";
+    } else if (dnaArray[i] === "T") {
+      dnaArray[i] = "A";
+    } else if (dnaArray[i] === "C") {
+      dnaArray[i] = "G";
+    } else {
+      dnaArray[i] = "C";
+    }
+  }
+  return dnaArray.join('');
+}
